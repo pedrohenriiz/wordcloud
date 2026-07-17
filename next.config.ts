0607bbd0589bfb822d96d96f510ae3ab20cee490
Pedro Henrique <pedro.avare01@gmail.com>
2026-07-17 18:49:08 -0300

    adds basepath

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath: '/wordcloud',
 };
 
 export default nextConfig;
